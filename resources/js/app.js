3e79511301f33aa53724f2c53022df778d243d20
angel-jeyriel <angeljeyriel@gmail.com>
2025-07-25 02:38:19 +0100

    Additional commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,17 @@ import Chart from 'chart.js/auto';
 window.Chart = Chart;
 
 // Notification handler
-// Livewire.on('open-notifications', () => {
-//     const messages = @json(auth() -> user() ? auth() -> user() -> unreadNotifications -> pluck('data.message') : []);
-//     alert('Notifications: ' + (messages.length ? messages.join('\n') : 'No new notifications'));
-//     if (messages.length) {
-//         fetch('/notifications/mark-as-read', {
-//             method: 'POST',
-//             headers: {
-//                 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
-//             },
-//         });
-//     }
-// });
+Livewire.on('open-notifications', () => {
+    const messages = @json(auth() -> user() ? auth() -> user() -> unreadNotifications -> pluck('data.message') : []);
+
+    alert('Notifications: ' + (messages.length ? messages.join('\n') : 'No new notifications'));
+
+    if (messages.length) {
+        fetch('/notifications/mark-as-read', {
+            method: 'POST',
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+            },
+        });
+    }
+});

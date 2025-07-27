8fe74f7e784ae72bd1a227eee705f4a59d70966d
angel-jeyriel <angeljeyriel@gmail.com>
2025-07-27 23:28:29 +0100

    New commit

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,6 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('homepage');
-// })->name('home')->middleware(['auth']);
-
-// Route::get('/welcome', function () {
-//     return view('welcome');
-// })->name('welcome');
-
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
